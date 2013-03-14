5c1418e3387b3b6865253d81d684eeb10140d959
Paul Gueller <paul.gueller@7summitsagency.com>
2013-03-14 11:23:39 -0500

    include link to github issues and wiki

--- webroot/config.rb
+++ webroot/config.rb
@@ -2,13 +2,13 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "styles"
+css_dir = "styles/"
 sass_dir = "styles/sass"
 images_dir = "images"
 javascripts_dir = "scripts"
 fonts_dir = "fonts"
 
-output_style = :expanded
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
